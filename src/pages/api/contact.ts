43fb122d7b5fe9993377c5b1455f30e86f4df258
Max Strootmann <max@nngrafischontwerp.nl>
2023-11-21 07:13:05 +0100

    change email address

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,31 +1,32 @@
-import { Resend } from 'resend';
-import ContactEmail from '../../emails/contactFormEmail';
-import { NextApiRequest, NextApiResponse } from 'next';
+import { Resend } from "resend";
+import ContactEmail from "../../emails/contactFormEmail";
+import { NextApiRequest, NextApiResponse } from "next";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async function POST(req: NextApiRequest, res: NextApiResponse) {
   try {
-    console.log("req.body: ", req.body);``
+    console.log("req.body: ", req.body);
+    ``;
     const { name, email, phoneNumber, message } = req.body;
 
-    if(!name || !email || !phoneNumber || !message) {
-      throw new Error('Incomplete request body');
+    if (!name || !email || !phoneNumber || !message) {
+      throw new Error("Incomplete request body");
     }
 
     await resend.sendEmail({
-      from: 'email@nngrafischontwerp.nl',
-      to: 'info@haaratelier-alkmaar.nl',
-      subject: 'Nieuwe contactaanvraag van haaratelier-alkmaar.nl',
-      react: ContactEmail({name, email, phoneNumber, message})
+      from: "email@nngrafischontwerp.nl",
+      to: "info@marloesotjes-haaratelier.nl",
+      subject: "Nieuwe contactaanvraag van haaratelier-alkmaar.nl",
+      react: ContactEmail({ name, email, phoneNumber, message }),
     });
 
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({ status: 'OK' }));
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify({ status: "OK" }));
   } catch (error) {
     console.error("Error sending email:", error);
 
-    res.setHeader('Content-Type', 'application/json');
-    res.status(500).end(JSON.stringify({ status: 'error', message: 'Failed to send email.' }));
+    res.setHeader("Content-Type", "application/json");
+    res.status(500).end(JSON.stringify({ status: "error", message: "Failed to send email." }));
   }
 }
