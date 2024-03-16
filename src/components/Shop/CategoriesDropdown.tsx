8c3667d52c58ee5283d67023adf01742f867bd60
Max Strootmann <max@nngrafischontwerp.nl>
2024-03-16 13:23:54 +0100

    fix build errors

--- src/components/Shop/CategoriesDropdown.tsx
+++ src/components/Shop/CategoriesDropdown.tsx
@@ -1,67 +1,48 @@
-import React, { Fragment, useState } from 'react'
-import { Listbox, Transition } from '@headlessui/react'
-import { CheckIcon, ChevronDownIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
-import { useRouter, useSearchParams } from 'next/navigation';
-import Link from 'next/link';
+import React, { Fragment, useState } from "react";
+import { Listbox, Transition } from "@headlessui/react";
+import { CheckIcon, ChevronDownIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
+import { useRouter, useSearchParams } from "next/navigation";
+import Link from "next/link";
 
 interface CategoriesDropdownProps {
   categories: any[];
 }
 
-const CategoriesDropdown: React.FC<CategoriesDropdownProps> = ({categories}) => {
-  
-  const [selected, setSelected] = useState(categories[categories.length - 1])
-  let selectedSort = useSearchParams().get("sort");
-  let sort = "";
-  if(selectedSort === null || "") {
-    sort = ""
-  } else sort = `&sort=${selectedSort}`;
+const CategoriesDropdown: React.FC<CategoriesDropdownProps> = ({ categories }) => {
+  const [selected, setSelected] = useState(categories[categories.length - 1]);
+  let selectedSort = useSearchParams()?.get("sort") ?? "";
+  let sort = selectedSort ? `&sort=${selectedSort}` : "";
 
   return (
-    <div className="">
+    <div className=''>
       <Listbox value={selected} onChange={setSelected}>
-        <div className="relative mt-1">
-          <Listbox.Button 
-          className="relative text-sm shadow-sm bg-bg-300 w-full cursor-default rounded-lg py-2 pl-2 pr-7 text-left ring-1 ring-black ring-opacity-5 focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-            <span className="block truncate">{selected}</span>
-            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-              <ChevronDownIcon
-                className="h-5 w-5 text-gray-500"
-                aria-hidden="true"
-              />
+        <div className='relative mt-1'>
+          <Listbox.Button className='relative text-sm shadow-sm bg-bg-300 w-full cursor-default rounded-lg py-2 pl-2 pr-7 text-left ring-1 ring-black ring-opacity-5 focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm'>
+            <span className='block truncate'>{selected}</span>
+            <span className='pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2'>
+              <ChevronDownIcon className='h-5 w-5 text-gray-500' aria-hidden='true' />
             </span>
           </Listbox.Button>
-          <Transition
-            as={Fragment}
-            leave="transition ease-in duration-100"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <Listbox.Options className="absolute z-10 rounded-xl mt-1 w-full md:w-max bg-bg-300 py-1 text-sm shadow-sm ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+          <Transition as={Fragment} leave='transition ease-in duration-100' leaveFrom='opacity-100' leaveTo='opacity-0'>
+            <Listbox.Options className='absolute z-10 rounded-xl mt-1 w-full md:w-max bg-bg-300 py-1 text-sm shadow-sm ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'>
               {categories.map((category, index) => (
                 <Link
-                scroll={false}
-                key={index}
-                href={`?category=${category.replace(/\s+/g, "-").replace(/&/g, "and")}${sort}`}>
+                  scroll={false}
+                  key={index}
+                  href={`?category=${category.replace(/\s+/g, "-").replace(/&/g, "and")}${sort}`}
+                >
                   <Listbox.Option
                     key={index}
                     className={({ active }) =>
                       `relative cursor-default select-none py-2 px-3 ${
-                        active ? 'bg-accent-500 text-white' : 'text-gray-900'
+                        active ? "bg-accent-500 text-white" : "text-gray-900"
                       }`
                     }
                     value={category}
                   >
                     {({ selected }) => (
                       <>
-                        <span
-                          className={`block truncate ${
-                            selected ? 'font-medium' : 'font-normal'
-                          }`}
-                        >
-                          {category}
-                        </span>
-                  
+                        <span className={`block truncate ${selected ? "font-medium" : "font-normal"}`}>{category}</span>
                       </>
                     )}
                   </Listbox.Option>
@@ -72,7 +53,7 @@ const CategoriesDropdown: React.FC<CategoriesDropdownProps> = ({categories}) => 
         </div>
       </Listbox>
     </div>
-  )
-}
+  );
+};
 
-export default CategoriesDropdown
+export default CategoriesDropdown;
